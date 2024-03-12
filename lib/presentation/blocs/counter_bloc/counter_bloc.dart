81d91ece850ebbe7f17e1a4be103c0360f0fd352
isaias-alt <cascolucasisaias@gmail.com>
2024-03-12 15:00:15 -0300

    feat(presentation): implement bloc

--- lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -7,6 +7,8 @@ part 'counter_state.dart';
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(const CounterState()) {
     on<CounterIncreased>(_onCounterIncreased);
+
+    on<CounterReset>(_onCounterReset);
   }
 
   void _onCounterIncreased(CounterIncreased event, Emitter<CounterState> emit) {
@@ -14,6 +16,14 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
       state.copyWith(
         counter: state.counter + event.value,
         transactionCount: state.transactionCount + 1,
+      ),
+    );
+  }
+
+  void _onCounterReset(CounterReset event, Emitter<CounterState> emit) {
+    emit(
+      state.copyWith(
+        counter: 0,
       ),
     );
   }
