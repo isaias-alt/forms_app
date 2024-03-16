f179e12c9bcf36c293d9667242654ebb5953562a
isaias-alt <cascolucasisaias@gmail.com>
2024-03-15 23:07:14 -0300

    refactor: add folders for more order

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,4 @@
-export 'home_screen.dart';
-export 'cubit_counter_screen.dart';
-export 'bloc_counter_screen.dart';
-export 'register_screen.dart';
+export 'home_screen/home_screen.dart';
+export 'cubit_counter_screen/cubit_counter_screen.dart';
+export 'bloc_couter_screen/bloc_counter_screen.dart';
+export 'register_screen/register_screen.dart';
